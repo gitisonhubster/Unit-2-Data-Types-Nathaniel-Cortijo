4849731d3d29e33014847222020cc3b7279b4818
gitisonhubster <Nathanielc140@nycstudents.net>
2026-09-24 10:13:18 -0400

    Update data.py

--- data.py
+++ data.py
@@ -1,5 +1,43 @@
-origninal_price=int((input("how much are you paying?")))
+""" x = 3
+y = float(3)
+print(x,y)
+
+
+ """
+
+
+
+""" origninal_price=int((input("how much are you paying?")))
 Tip_amount=int((input("what percent are you tiping")))
 print("Original Price:" , origninal_price)
 print("Tip:" , Tip_amount)
-print("Total:" , origninal_price+(origninal_price*(Tip_amount/100)))
+print("Total:" , origninal_price+(origninal_price*(Tip_amount/100))) """
+
+
+""" 
+
+
+values = [1,2.23,5,7,2,30,15] """
+""" print(values)
+for i in values:
+    print(i) """
+""" print(values[0])
+print(values[6]) """
+
+
+""" x = "this is a thing"
+y= x.split( )
+z = y[0]
+print(y)
+print(z) """
+
+
+""" x = input("gimme a sentence")
+y = len(x.split( ))
+print(y) """
+
+day_of_week = input("what day is it? ")
+if day_of_week == "Friday":
+    print("correct")
+else:
+    print("incorrect")
